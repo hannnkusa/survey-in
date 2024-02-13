90be5e0b687c08838684e1a5d70ede6935dad7d1
Handy Kusuma <55536502+hannnkusa@users.noreply.github.com>
2024-02-12 20:48:08 -0800

    Bugfix/fixing buggy app (#8)
    * created major feature
    
    * major update
    
    * fixed typescript error
    
    * updated gitignore
    
    * fixed a whole app
    
    * fixed reload
    
    * fixed minor
    
    * fixed minor bug
    
    * deleted unused
    
    ---------
    
    Co-authored-by: hankusa <handy.kusuma@roketin.com>

--- src/components/pages/Create/_components/RespondentDetail/index.types.ts
+++
@@ -1,7 +0,0 @@
-import { UseDisclosureReturn } from "@chakra-ui/react";
-
-export interface Props {
-    respondentType: string;
-}
-
-export type RespondentSelectionProps = UseDisclosureReturn & Props;
