5720a38cf2c9f35a19dc356660394d09c009dbd0
Handy Kusuma <55536502+hannnkusa@users.noreply.github.com>
2024-02-14 08:47:08 -0800

    Dev (#14)
    * rollback and fixed the api that caused from error dynamic params
    
    * fixed git wrong merging
    
    ---------
    
    Co-authored-by: hankusa <handy.kusuma@roketin.com>

--- src/app/api/get-google-form-title/route.ts
+++ src/app/api/get-google-form-title/route.ts
@@ -1,21 +1,31 @@
 import { NextResponse, type NextRequest } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
+export const dynamicParams = true // true | false,
+
 export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
     const url = searchParams.get("url");
-    
+
+    if (!!url) {
+      return NextResponse.json({ error: "URL is required" }, { status: 422 });
+    }
+
     // Fetch the Google Form response
     const response = await fetch(url as string);
-    
+
     // Check if the response is successful
     if (!response.ok) {
-      throw new Error(`Failed to fetch Google Form response: ${response.statusText}`);
+      throw new Error(
+        `Failed to fetch Google Form response: ${response.statusText}`
+      );
     }
-    
+
     // Read the response body as text
     const responseData = await response.text();
-    
+
     // Extract the title from the HTML response
     const titleMatch = responseData.match(/<title>(.*?)<\/title>/);
     const title = titleMatch ? titleMatch[1] : "Form Title Not Found";
@@ -28,7 +38,7 @@ export async function GET(req: NextRequest) {
       },
     });
   } catch (error) {
-    console.error('Error fetching Google Form response:', error);
+    console.error("Error fetching Google Form response:", error);
     return NextResponse.error();
   }
 }
