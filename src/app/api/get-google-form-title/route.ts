eefe865f5811ab53a974e2e641cf806eda38e34b
hankusa <handy.kusuma@roketin.com>
2024-02-14 21:09:33 +0700

    fixed get google form title that is failure on deployment

--- src/app/api/get-google-form-title/route.ts
+++ src/app/api/get-google-form-title/route.ts
@@ -1,21 +1,34 @@
 import { NextResponse, type NextRequest } from "next/server";
-import axios from "axios";
 
 export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
     const url = searchParams.get("url");
-    const response = await axios.get(url as string);
-    const titleMatch = response.data.match(/<title>(.*?)<\/title>/);
+    
+    // Fetch the Google Form response
+    const response = await fetch(url as string);
+    
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error(`Failed to fetch Google Form response: ${response.statusText}`);
+    }
+    
+    // Read the response body as text
+    const responseData = await response.text();
+    
+    // Extract the title from the HTML response
+    const titleMatch = responseData.match(/<title>(.*?)<\/title>/);
     const title = titleMatch ? titleMatch[1] : "Form Title Not Found";
+
     return NextResponse.json({
-      message: "Successfully retrieved google form",
+      message: "Successfully retrieved Google Form",
       data: {
         url: url,
-        title,
+        title: title,
       },
     });
   } catch (error) {
+    console.error('Error fetching Google Form response:', error);
     return NextResponse.error();
   }
 }
