7017b94829d8b5dc0bf44b6d5675ec148ed138d2
Handy Kusuma <55536502+hannnkusa@users.noreply.github.com>
2024-02-24 05:32:19 -0800

    Dev (#6)

--- src/utils/hooks/useFcmToken.tsx
+++ src/utils/hooks/useFcmToken.tsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react";
-import { getToken } from "firebase/messaging";
-import { messaging } from "@/firebase/config";
+// import { useEffect, useState } from "react";
+// import { getToken } from "firebase/messaging";
+// import { messaging } from "@/firebase/config";
 
-const useFcmToken = () => {
-  const [token, setToken] = useState("");
-  const [notificationPermissionStatus, setNotificationPermissionStatus] =
-    useState("");
+// const useFcmToken = () => {
+//   const [token, setToken] = useState("");
+//   const [notificationPermissionStatus, setNotificationPermissionStatus] =
+//     useState("");
 
-  useEffect(() => {
-    const retrieveToken = async () => {
-      try {
-        if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-          // Retrieve the notification permission status
-          const permission = await Notification.requestPermission();
-          setNotificationPermissionStatus(permission);
+//   useEffect(() => {
+//     const retrieveToken = async () => {
+//       try {
+//         if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+//           // Retrieve the notification permission status
+//           const permission = await Notification.requestPermission();
+//           setNotificationPermissionStatus(permission);
 
-          // Check if permission is granted before retrieving the token
-          if (permission === "granted") {
-            const currentToken = await getToken(messaging, {
-              vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
-            });
-            if (currentToken) {
-              setToken(currentToken);
-            } else {
-              console.log(
-                "No registration token available. Request permission to generate one."
-              );
-            }
-          }
-        }
-      } catch (error) {
-        console.log("An error occurred while retrieving token:", error);
-      }
-    };
+//           // Check if permission is granted before retrieving the token
+//           if (permission === "granted") {
+//             const currentToken = await getToken(messaging, {
+//               vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+//             });
+//             if (currentToken) {
+//               setToken(currentToken);
+//             } else {
+//               console.log(
+//                 "No registration token available. Request permission to generate one."
+//               );
+//             }
+//           }
+//         }
+//       } catch (error) {
+//         console.log("An error occurred while retrieving token:", error);
+//       }
+//     };
 
-    retrieveToken();
-  }, []);
+//     retrieveToken();
+//   }, []);
 
-  return { fcmToken: token, notificationPermissionStatus };
-};
+//   return { fcmToken: token, notificationPermissionStatus };
+// };
 
-export default useFcmToken;
+// export default useFcmToken;
