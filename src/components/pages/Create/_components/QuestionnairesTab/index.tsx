b81d76f0751deb3bbd8479c954e02256fc47b25a
hankusa <handy.kusuma@roketin.com>
2024-02-25 13:01:08 +0700

    changed iframe to embed

--- src/components/pages/Create/_components/QuestionnairesTab/index.tsx
+++ src/components/pages/Create/_components/QuestionnairesTab/index.tsx
@@ -27,11 +27,7 @@ export default function QuestionnairesTab({
 }: questionnairesTabProps) {
   const questionnaire = submittedUrl ? (
     <Flex gap="2vw" w="100%">
-      <iframe
-        sandbox="allow-same-origin allow-scripts allow-forms"
-        className={styles.gdocsIframe}
-        src={submittedUrl}
-      />
+      <embed className={styles.gdocsIframe} src={submittedUrl} />
     </Flex>
   ) : (
     <form onSubmit={onSubmitForm}>
